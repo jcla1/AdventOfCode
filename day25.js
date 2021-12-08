7c371cbace92a222133368d60e2d5fb563afa89e
Joseph Adams <whitegolem@gmail.com>
2021-12-08 21:20:33 +0100

    initial commit. Add day 25 solution.

---
+++ day25.js
@@ -0,0 +1,21 @@
+const R = require('ramda');
+const fs = require('fs');
+
+const input = fs.readFileSync('day25.input', 'utf8').trim();
+const [cardPK, doorPK] = R.compose(R.map(parseInt), R.split('\n'))(input);
+
+const base      = 20201227,
+      transform = (n, subjectNum = 7) => (n * subjectNum) % base;
+
+const transformPK = (loopSize, subjectNum = 7) => R.reduce(n => transform(n, subjectNum), 1, R.range(0, loopSize));
+
+const findLoopSize = pk => {
+  var candidate = 1;
+  for (var loopSize = 1; true; loopSize++) {
+    candidate = transform(candidate);
+    if (candidate === pk) return loopSize;
+  }
+}
+
+const encryptionKey = transformPK(findLoopSize(doorPK), cardPK);
+console.log(encryptionKey);
