1ed683e846d5a7bece7d888173d11d02c7f4f421
Joseph Adams <whitegolem@gmail.com>
2021-12-21 16:37:01 +0100

    Pull util.js from AOC2020.

--- util.js
+++ util.js
@@ -76,6 +76,10 @@ const getNeighbours = R.curry((board, [i, j], diag = false) =>
       R.concat([[i-1, j], [i, j-1], [i, j+1], [i+1, j]],
             diag ? [[i-1, j-1], [i-1, j+1], [i+1, j-1], [i+1, j+1]] : [])));
 
+// findIndicies :: (a -> Bool) -> [[a]] -> [(Int, Int)]
+const findIndicies = R.curry((f, board) =>
+  R.filter((p) => f(get(board, p)), getIndicies(board)));
+
 const pad = (img, n = 2, item = '.') => {
   const width = R.length(img[0]);
   const topBottom = R.map((_) => R.repeat(item, width + 2*n), R.range(0, n));
@@ -103,6 +107,6 @@ module.exports = {
   get, set,
   onBoard, mapBoard, addBoards, sumBoard, zerosLike,
   boardToString, printBoard,
-  getIndicies, getNeighbours,
+  getIndicies, getNeighbours, findIndicies,
   pad, aperture2D,
 };
