75d5083a69b85fc5d73befa870a43cc2b3a1fab6
Joseph Adams <whitegolem@gmail.com>
2022-12-06 08:20:54 +0100

    Fix ESLint complaining about day 4.

--- AOC2022/day04.js
+++ AOC2022/day04.js
@@ -5,25 +5,28 @@ const U = require('./util.js');
 
 const input = fs.readFileSync('inputs/day04.input', 'utf8').trim();
 
-const contained = (l, r) => (l[0] <= r[0] && l[1] >= r[1]) || (l[0] >= r[0] && l[1] <= r[1]);
-const overlap = (l, r) => contained(l, r) || (r[0] <= l[0] && l[0] <= r[1]) || (r[0] <= l[1] && l[1] <= r[1]);
+const contained = (l, r) => (l[0] <= r[0] && l[1] >= r[1]) ||
+                            (l[0] >= r[0] && l[1] <= r[1]);
+const overlap = (l, r) => contained(l, r) ||
+                          (r[0] <= l[0] && l[0] <= r[1]) ||
+                          (r[0] <= l[1] && l[1] <= r[1]);
 
 const assignments = R.compose(
-  R.map(R.compose(
-    R.splitAt(2),
-    U.toIntArr,
-    R.take(4),
-    R.drop(1),
-    R.match(/([0-9]+)-([0-9]+),([0-9]+)-([0-9]+)/))),
-  R.split('\n'))(input);
+    R.map(R.compose(
+        R.splitAt(2),
+        U.toIntArr,
+        R.take(4),
+        R.drop(1),
+        R.match(/([0-9]+)-([0-9]+),([0-9]+)-([0-9]+)/))),
+    R.split('\n'))(input);
 
 const numEnclosed = R.compose(
-  R.length,
-  R.filter(R.apply(contained)))(assignments);
+    R.length,
+    R.filter(R.apply(contained)))(assignments);
 
 const numOverlap = R.compose(
-  R.length,
-  R.filter(R.apply(overlap)))(assignments);
+    R.length,
+    R.filter(R.apply(overlap)))(assignments);
 
 console.log(numEnclosed);
 console.log(numOverlap);
