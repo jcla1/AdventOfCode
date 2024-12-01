7dad46aad767b816d7dbfd682efc99c69e2072ac
Joseph Adams <whitegolem@gmail.com>
2024-12-01 17:56:53 +0100

    Solve first day of AOC 2024.

--- AOC2024/day01.js
+++ AOC2024/day01.js
@@ -4,3 +4,27 @@ const fs = require('fs');
 const U = require('./util.js');
 
 const input = fs.readFileSync('inputs/day01.input', 'utf8').trim();
+const nums = R.compose(
+  R.transpose,
+  R.map(R.map((n) => parseInt(n))),
+  R.map(R.split(/\s+/)),
+  R.split('\n'))(input);
+
+const total = R.compose(
+  R.sum,
+  R.apply(R.zipWith((a, b) => Math.abs(a - b))),
+  R.map(R.sort((a, b) => a - b)))(nums);
+
+console.log(total);
+
+const freq = R.compose(
+  R.map(R.length),
+  R.groupBy(R.identity),
+  R.nth(1))(nums);
+
+const sum = R.compose(
+  R.sum,
+  R.map((n) => freq[n] ? freq[n] * n : 0),
+  R.nth(0))(nums);
+
+console.log(sum);
