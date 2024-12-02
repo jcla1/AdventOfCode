5c615a855342ced8744c050c2bad5de25dc438d5
Joseph Adams <whitegolem@gmail.com>
2024-12-02 13:35:08 +0100

    Fix JS using eslint.

--- AOC2016/day01.js
+++ AOC2016/day01.js
@@ -9,10 +9,10 @@ const turn = (facing, dir) => ({
   'N': {'L': 'W', 'R': 'E'},
   'E': {'L': 'N', 'R': 'S'},
   'S': {'L': 'E', 'R': 'W'},
-  'W': {'L': 'S', 'R': 'N'}
+  'W': {'L': 'S', 'R': 'N'},
 }[facing][dir]);
 
-const advanceState = ({dir: facing, x: x, y: y, visited: visited}, [dir, dist]) => {
+const advanceState = ({dir: facing, x, y, visited}, [dir, dist]) => {
   const newDir = turn(facing, dir);
   const newVisited = [...visited, ...R.range(1, dist + 1).map((n) => {
     switch (newDir) {
@@ -28,18 +28,18 @@ const advanceState = ({dir: facing, x: x, y: y, visited: visited}, [dir, dist]) 
 };
 
 const endWalkState = R.compose(
-  R.reduce(advanceState, {x: 0, y: 0, dir: 'N', visited: []}),
-  R.map((d) => [d[0], parseInt(d.slice(1))]),
-  R.split(', '))(input);
+    R.reduce(advanceState, {x: 0, y: 0, dir: 'N', visited: []}),
+    R.map((d) => [d[0], parseInt(d.slice(1))]),
+    R.split(', '))(input);
 
 console.log(distance([endWalkState.x, endWalkState.y]));
 
 const firstDuplicate = R.compose(
-  JSON.parse,
-  R.nth(0),
-  R.head,
-  R.filter((n) => n[1] > 1),
-  R.toPairs,
-  R.countBy(R.toString));
+    JSON.parse,
+    R.nth(0),
+    R.head,
+    R.filter((n) => n[1] > 1),
+    R.toPairs,
+    R.countBy(R.toString));
 
 console.log(distance(firstDuplicate(endWalkState.visited)));
