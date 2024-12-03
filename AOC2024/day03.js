4740f2e34bae0001e073b2d9e298042a9dbcf372
Joseph Adams <whitegolem@gmail.com>
2024-12-03 08:05:06 +0100

    Solve day 03 in AOC 2024.

--- AOC2024/day03.js
+++ AOC2024/day03.js
@@ -2,3 +2,22 @@ const R = require('ramda');
 const U = require('./util.js');
 
 const input = U.getInput(__filename);
+
+const addUpMuls = R.compose(
+  R.sum,
+  R.map(R.compose(
+    R.apply(R.multiply),
+    U.toIntArr,
+    R.match(/\d{1,3}/g))),
+  R.match(/mul\((\d{1,3}),(\d{1,3})\)/g))
+
+console.log(addUpMuls(input));
+
+const conditionalRes = R.compose(
+  R.sum,
+  R.map(addUpMuls),
+  R.unnest,
+  R.map(R.drop(1)),
+  R.map(R.split("do()")),
+  R.split("don't()"))("do()" + input);
+console.log(conditionalRes);
