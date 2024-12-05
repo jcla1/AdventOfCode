214726af32ad5b46110d7cf8cb65127b6e929c30
Joseph Adams <whitegolem@gmail.com>
2024-12-05 12:23:41 +0100

    Solve day 14 of AOC 2016, but part 2 is _very_ slow.

--- AOC2016/day14.js
+++ AOC2016/day14.js
@@ -3,5 +3,53 @@ const U = require('./util.js');
 const crypto = require('crypto');
 
 const input = 'jlmsuwbz';
+// const input = 'abc';
 
-const getMD5 = (s) => crypto.createHash('md5').update(s).digest('hex');
+const getMD5 = (s, streched = false) => {
+  if (!streched) return crypto.createHash('md5').update(s).digest('hex');
+  let hash = s;
+  for (let i = 0; i <= 2016; i++) {
+    hash = crypto.createHash('md5').update(hash).digest('hex');
+  }
+  return hash;
+};
+
+const matchThree = (s) => {
+  const m = s.match(/(.)\1\1/);
+  return m ? m[1] : null;
+};
+
+const matchFive = (s, c) => R.includes(R.repeat(c, 5).join(''), s);
+
+const lastKeyIndex = (salt, streched = false) => {
+  let i = 0;
+  let foundKeys = 0;
+  let hashCache = [];
+
+  const getHash = (i) => {
+    if (!hashCache[i]) return getMD5(salt + i, streched);
+    return hashCache[i];
+  };
+
+  outer: while (foundKeys < 64) {
+    console.log('checking index', i);
+    const hash = getHash(i);
+    const m = matchThree(hash);
+    if (m) {
+      for (let j = 1; j <= 1000; j++) {
+        const hash2 = getHash(i + j);
+        if (matchFive(hash2, m)) {
+          console.log(i, i+j, m, hash, hash2);
+          foundKeys++;
+          i++;
+          continue outer;
+        }
+      }
+    }
+    i++;
+  }
+  return i - 1;
+};
+
+// console.log(lastKeyIndex(input));
+console.log(lastKeyIndex(input, true));
