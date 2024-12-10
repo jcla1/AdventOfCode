afb54b657bcc16d8003aea154dec3698a7b8ca7b
Joseph Adams <whitegolem@gmail.com>
2024-12-10 17:24:53 +0100

    Solve day 9 in AOC 2024.

---
+++ AOC2024/day09.js
@@ -0,0 +1,74 @@
+const R = require('ramda');
+const U = require('./util.js');
+
+const input = U.getInput(__filename);
+
+const tn = (n) => n * (n + 1) / 2;
+
+const splitDiscMap = R.compose(
+    R.transpose,
+    R.splitEvery(2),
+    U.toIntArr,
+    R.split(''));
+
+const getDiscChecksum = (discMap) => {
+  const [files, spaces] = splitDiscMap(discMap);
+
+  let checksum = 0;
+  let curIndex = 0;
+  outer: for (let fileNo = 0; fileNo < files.length; fileNo++) {
+    const fileLen = files[fileNo];
+
+    checksum += fileNo * (tn(curIndex + fileLen - 1) - tn(curIndex - 1));
+    curIndex += fileLen;
+
+    while (spaces[fileNo] > 0) {
+      const fillerFileID = R.findLastIndex(R.lt(0), files);
+      if (fillerFileID === -1 || fillerFileID <= fileNo) break outer;
+
+      checksum += fillerFileID * curIndex;
+
+      curIndex++;
+      files[fillerFileID]--;
+      spaces[fileNo]--;
+    }
+  }
+
+  return checksum;
+};
+console.log(getDiscChecksum(input));
+
+const getChunkedChecksum = (discMap) => {
+  const [files, spaces] = splitDiscMap(discMap);
+  const origFileLen = R.clone(files);
+
+  let checksum = 0;
+  let curIndex = 0;
+  outer: for (let fileNo = 0; fileNo < files.length; fileNo++) {
+    const fileLen = files[fileNo];
+
+    //console.log('file', fileNo, 'length after move', fileLen, 'followed by no. spaces', spaces[fileNo]);
+    checksum += fileNo * (tn(curIndex + fileLen - 1) - tn(curIndex - 1));
+    curIndex += Math.max(fileLen, origFileLen[fileNo]);
+
+    const nextMovableFileIndex = () => fileNo + R.findLastIndex(R.allPass([R.lt(0), R.gte(spaces[fileNo])]), files.slice(fileNo));
+
+    while (spaces[fileNo] > 0 && nextMovableFileIndex() > fileNo) {
+      const fillerFileID = nextMovableFileIndex();
+      const fillerLen = files[fillerFileID];
+      //console.log('can move file', fillerFileID, 'to', curIndex, 'with length', fillerLen);
+
+      checksum += fillerFileID * (tn(curIndex + fillerLen - 1) - tn(curIndex - 1));
+
+      curIndex += fillerLen;
+      files[fillerFileID] = 0;
+      spaces[fileNo] -= fillerLen;
+    }
+    curIndex += spaces[fileNo];
+    //console.log('ausgleich von', spaces[fileNo], 'spaces');
+    //console.log('-----');
+  }
+
+  return  checksum;
+};
+console.log(getChunkedChecksum(input));
