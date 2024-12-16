4d0e992741659724a7613bfa383be588bb370886
Joseph Adams <whitegolem@gmail.com>
2024-12-16 11:39:10 +0100

    Solve day 16 of AOC 2024.

---
+++ AOC2024/day16.js
@@ -0,0 +1,108 @@
+const R = require('ramda');
+const U = require('./util.js');
+
+const input = U.getInput(__filename);
+
+const matchLocation = (grid, c) => {
+  const row = R.findIndex(R.includes(c), grid);
+  const col = R.findIndex(R.equals(c), grid[row]);
+  return [row, col];
+};
+
+const grid = R.compose(
+  R.map(R.split('')),
+  R.split('\n'))(input);
+
+const startPos = matchLocation(grid, 'S');
+const startState = [startPos, 'E'];
+
+const endPos = matchLocation(grid, 'E');
+
+const getNeighbours = (grid, posState) => {
+  const [[row, col], face] = posState;
+  let neighbours = [];
+
+  switch (face) {
+    case 'N':
+      neighbours = [[[row - 1, col], 'N'], [[row, col], 'E'], [[row, col], 'W']];
+      break;
+    case 'E':
+      neighbours = [[[row, col], 'S'], [[row, col + 1], 'E'], [[row, col], 'N']];
+      break;
+    case 'S':
+      neighbours = [[[row + 1, col], 'S'], [[row, col], 'W'], [[row, col], 'E']];
+      break;
+    case 'W':
+      neighbours = [[[row, col], 'N'], [[row, col - 1], 'W'], [[row, col], 'S']];
+      break;
+  }
+
+  return R.filter(([[r, c], f]) =>
+       r >= 0
+    && r < grid.length
+    && c >= 0
+    && c < grid[0].length
+    && grid[r][c] != '#', neighbours);
+};
+
+const stepCost = (curState, nextState) => {
+  if (curState[1] != nextState[1]) return 1000;
+  return 1;
+};
+
+const isAtEnd = (state) => R.equals(state[0], endPos);
+
+const getPathCost = R.compose(
+  R.sum,
+  R.map(R.apply(stepCost)),
+  R.aperture(2));
+
+const bestPath = U.astar(
+  startState,
+  isAtEnd,
+  R.curry(getNeighbours)(grid),
+  R.always(0),
+  stepCost);
+const bestPathCost = getPathCost(bestPath);
+console.log(bestPathCost);
+
+// not the prettiest solution to brute force from every possible alternate
+// neighbour, but it is correct. Unfortunately it's also quite slow.
+const findAlternatePaths = (grid, shortestPath, maxCost) => {
+  let paths = [shortestPath];
+
+  for (let i = 0; i < shortestPath.length - 1; i++) {
+    const curState = shortestPath[i];
+    const neighbourStates = getNeighbours(grid, curState);
+    if (neighbourStates.length == 1) continue;
+
+    const nextState = shortestPath[i + 1];
+    const alternateNeighbours = R.without([nextState], neighbourStates);
+    const alternatePathCompletions = R.map((n) => {
+      return U.astar(
+        n,
+        isAtEnd,
+        R.curry(getNeighbours)(grid),
+        R.always(0),
+        stepCost);
+    }, alternateNeighbours);
+
+    const alternatePaths = R.map((p) => [...shortestPath.slice(0, i + 1), ...p], alternatePathCompletions);
+    const alternateCosts = R.map(getPathCost, alternatePaths);
+
+    for (let j = 0; j < alternatePaths.length; j++) {
+      if (alternateCosts[j] <= maxCost) {
+        paths.push(alternatePaths[j]);
+      }
+    }
+  }
+
+  return paths;
+};
+
+const paths = findAlternatePaths(grid, bestPath, bestPathCost);
+const allTiles = R.compose(
+  R.uniq,
+  R.map(R.head),
+  R.unnest)(paths);
+console.log(R.length(allTiles));
--- AOC2024/util.js
+++ AOC2024/util.js
@@ -2,6 +2,8 @@ const R = require('ramda');
 const path = require('path');
 const fs = require('fs');
 
+const {Heap} = require('heap-js');
+
 const getInput = (f) => {
   const inputPath = 'inputs/' + path.basename(f).replace('.js', '.input');
   return fs.readFileSync(inputPath, 'utf8').trim();
@@ -25,6 +27,65 @@ const aperture2D = R.curry(([n, k], grid) => {
       R.map(R.aperture(k)))(grid);
 });
 
+// A* search algorithm. Given an abstract initial state and goal state, a
+// function that, given a state, returns a list of possible next states, and a
+// heuristic function that estimates the cost of getting between two states,
+// returns the sequence of states that leads from the initial state to the goal
+// state with the lowest cost.
+//
+// The goal can either be a function that returns true when a given state is the
+// goal, or a specific state. If a specific state is given, states will be
+// compared with R.equals.
+//
+// Not using a proper heurisitic function will reduce this to Dijkstra's
+// algorithm, which is the default.
+const astar = (initialState, goal, nextStates, heuristic = R.always(0), cost = R.always(1)) => {
+  const reconstructPath = (cameFrom, current) => {
+    if (R.equals(current, initialState)) return [current];
+    return [...reconstructPath(cameFrom, cameFrom[current]), current];
+  };
+
+  const areDone = (state) => typeof goal === 'function' ? goal(state) : R.equals(state, goal);
+
+  // Items in the heap are tuples of [fScore, node], so the comparison function
+  // is the comparison of fScores.
+  const openSet = new Heap((a, b) => a[0] - b[0]);
+  openSet.push([heuristic(initialState), initialState]);
+
+  // Map we need for reconstructing the shorest path.
+  const cameFrom = {};
+
+  // We don't need an fScore, as that's hidden in the MinHeap
+  const gScore = {};
+  gScore[initialState] = 0;
+
+  while (openSet.length !== 0) {
+    // we discard the fScore when popping from the heap
+    const [, node] = openSet.pop();
+    if (areDone(node)) return reconstructPath(cameFrom, node);
+
+    const neighbours = nextStates(node);
+    for (let i = 0; i < neighbours.length; i++) {
+      const neighbour = neighbours[i];
+
+      const tentativeGScore = gScore[node] + cost(node, neighbour);
+      if (tentativeGScore < (gScore[neighbour] || Infinity)) {
+        cameFrom[neighbour] = node;
+        gScore[neighbour] = tentativeGScore;
+
+        // Not sure why the pseudo-code implementation of this on Wikipedia
+        // conditions the addition of the neighbour to the heap on if it's
+        // already present.
+        // If it is, but with a higher cost/score than this one, then it will
+        // simply be ignored if we're just trying to solve the point-to-point
+        // problem (and there is in fact a path between them).
+        openSet.push([tentativeGScore + heuristic(neighbour), neighbour]);
+      }
+    }
+  }
+  console.error('No path found.');
+};
+
 module.exports = {
   getInput,
   isUpperCase,
@@ -32,4 +93,5 @@ module.exports = {
   sign, maximum, minimum,
   toIntArr, reduce1,
   aperture2D,
+  astar,
 };
