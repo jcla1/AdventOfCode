500e91900044f65da51872f1416648ef801aa3f2
Joseph Adams <whitegolem@gmail.com>
2025-12-04 11:10:57 +0100

    Solve part 2 of day 3 in AOC 2025.

--- AOC2025/day03.js
+++ AOC2025/day03.js
@@ -3,27 +3,23 @@ const U = require('./util.js');
 
 const input = U.getInput(__filename);
 
-const findLargestIndex = (xs) => {
-  let largestIndex = 0;
-  let largestValue = xs[0];
-  for (let i = 1; i < xs.length; i++) {
-    if (xs[i] > largestValue) {
-      largestValue = xs[i];
-      largestIndex = i;
-    }
-  }
-  return largestIndex;
-};
+const findLargestIndex = (xs, left, right) => left + xs.findIndex(Math.max(...xs.slice(left, right));
 
 const batteries = R.compose(
   R.map(R.o(U.toIntArr, R.split(''))),
   R.split('\n'))(input);
 
 const findJoltage = (n) => (b) => {
-  const idx = findLargestIndex(b.slice(0, -1));
-  const idx2 = findLargestIndex(b.slice(idx + 1));
-  console.log([idx, idx2]);
-  return 10 * b[idx] + b[idx + 1 + idx2];
+  let joltage = [];
+
+  let i = 0;
+  while (joltage.length < n) {
+    const idxOfLargest = findLargestIndex(b, i, b.length - (n - joltage.length - 1));
+    joltage.push(b[idxOfLargest]);
+    i = idxOfLargest + 1;
+  }
+
+  return parseInt(joltage.join(''));
 };
 
 const totalJoltage = (n) => R.compose(
