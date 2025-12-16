77df06b276ec511e1e67e41a8f890fef81ff5ae0
Joseph Adams <whitegolem@gmail.com>
2025-12-16 13:47:30 +0100

    Solve part 2 of day 10 in AOC 2025.

--- AOC2025/day10.js
+++ AOC2025/day10.js
@@ -44,13 +44,54 @@ const minNumPressesForLights = R.compose(
     nextStates(applyButtonForLight)(buttons))))(lines);
 console.log(minNumPressesForLights);
 
-const applyButtonForJoltages = (bt) => (s) => R.addIndex(R.map)((v, i) => bt.includes(i) ? v + 1 : v, s);
+// again, ugly as hell with the async init
+(async () => {
 
-const minNumPressesForJoltages = R.compose(
-  R.sum,
-  R.map(R.o(R.dec, R.length)),
-  R.map(({joltages, buttons}) => U.astar(
-    new Array(joltages.length).fill(0),
-    joltages,
-    nextStates(applyButtonForJoltages)(buttons))))(lines);
+const { Context } = await require('z3-solver').init();
+
+const findMinButtonPresses = async ({ buttons, joltages }) => {
+  const Z3 = Context('main');
+  const solver = new Z3.Optimize();
+  const presses = Array.from({ length: buttons.length }, (_, i) => Z3.Int.const(`btn_${i}`));
+
+  // Can't press a button a negative number of times
+  for (let i = 0; i < presses.length; i++) {
+    solver.add(presses[i].ge(0));
+  }
+
+  const resJoltages = Array.from({ length: joltages.length }, (_, j) => Z3.Int.val(0));
+  for (let k = 0; k < joltages.length; k++) {
+    for (let i = 0; i < buttons.length; i++) {
+      if (buttons[i].includes(k)) {
+        // each button press increases the joltage by 1
+        resJoltages[k] = resJoltages[k].add(presses[i]);
+      }
+    }
+
+    solver.add(resJoltages[k].eq(joltages[k]));
+  }
+
+
+  // we want the minimal number of button presses to reach each joltage
+  let noTotalPresses = Z3.Int.val(0);
+  for (let i = 0; i < presses.length; i++) {
+    noTotalPresses = noTotalPresses.add(presses[i]);
+  }
+  solver.minimize(noTotalPresses);
+
+  await solver.check();
+
+  const result = [];
+  for (let i = 0; i < presses.length; i++) {
+    result.push(solver.model().eval(presses[i]).toString());
+  }
+  return R.sum(U.toIntArr(result));
+};
+
+let minNumPressesForJoltages = 0;
+for (let i = 0; i < lines.length; i++) {
+  minNumPressesForJoltages += await findMinButtonPresses(lines[i]);
+}
 console.log(minNumPressesForJoltages);
+
+})();
